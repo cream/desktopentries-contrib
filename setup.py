258f70ef3de3876af1f66c69e18aba7ae05c2af4
Friedrich Weber <fred@reichbier.de>
2010-06-09 16:47:30 +0200

    Forgot setup.py >:O

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = 'Desktop entries Contrib Libraries',
+    version = '0.1',
+    package_dir = {'cream.contrib.desktopentries': 'desktopentries'},
+    packages = ['cream.contrib.desktopentries'],
+    )
